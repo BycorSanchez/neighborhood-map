6de3bdb19b691669e7fca26f97d98137e9cd459a
Bycor Sánchez <bycor1989@gmail.com>
2018-07-29 10:32:53 +0100

    refactor: remove embedded CSS

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,71 +1,42 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
-import Hidden from '@material-ui/core/Hidden';
 import Divider from '@material-ui/core/Divider';
 
-const sidebarWidth = 240;
-
-const styles = theme => ({
-    root: {
-        flexGrow: 1,
-        height: 430,
-        zIndex: 1,
-        overflow: 'hidden',
-        position: 'relative',
-        display: 'flex',
-        width: '100%',
-    },
-    drawerPaper: {
-        width: sidebarWidth,
-        [theme.breakpoints.up('md')]: {
-            position: 'relative',
-        },
-    }
-});
-
 const SideBar = props => {
-    const { classes, mobileOpen, onSidebarClose } = props;
+    const { mobileOpen, onSidebarClose } = props;
 
     const sidebar = (
-        <div>
+        <div className="sidebar">
             <Divider />
             <List>{"Places"}</List>
         </div>
     );
 
     return (
-        <div>
-            <Hidden mdUp>
-                <Drawer
-                    variant="temporary"
-                    anchor="left"
-                    open={mobileOpen}
-                    onClose={onSidebarClose}
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                >
-                    {sidebar}
-                </Drawer>
-            </Hidden>
-            <Hidden smDown implementation="css">
-                <Drawer
-                    variant="permanent"
-                    open
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                >
-                    {sidebar}
-                </Drawer>
-            </Hidden>
+        <div className="flex-grow">
+            <Drawer
+                variant="temporary"
+                anchor="left"
+                open={mobileOpen}
+                onClose={onSidebarClose}
+                className="drawer-mobile"
+                ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                }}
+            >
+                {sidebar}
+            </Drawer>
+
+            <Drawer
+                variant="permanent"
+                open
+                className="drawer-desktop"
+            >
+                {sidebar}
+            </Drawer>
         </div>
     );
 }
 
-export default withStyles(styles, { withTheme: true })(SideBar);
+export default SideBar;
