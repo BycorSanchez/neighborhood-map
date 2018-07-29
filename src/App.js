3e4a5f962c1e797dd3cbca308d8662b89db40afa
Bycor Sánchez <bycor1989@gmail.com>
2018-07-29 11:28:22 +0100

    feat: add map component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './Header';
-import SideBar from './SideBar';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import Map from "./Map";
 
 class App extends Component {
   state = {
     mobileOpen: false,
+    map: ''
   };
+
+  componentDidMount() {
+    this.loadMap();
+  }
 
   toggleSidebar = () => {
     this.setState(state => ({ mobileOpen: !state.mobileOpen }));
   };
+
+  loadMap() {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBo3lCapdQsXacp3sci5KKyz2rbCJh3AR0&callback=initMap";
+    script.onerror = error => console.error("Faile to load Map", error);
+
+    window.initMap = this.initMap;
+    document.head.appendChild(script);
+  };
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 40.7413549, lng: -73.99802439999996 },
+      zoom: 14
+    });
+
+    this.setState({ map });
+  }
 
   render() {
     const { mobileOpen } = this.state;
@@ -22,6 +47,7 @@ class App extends Component {
           mobileOpen={mobileOpen}
           onSidebarClose={this.toggleSidebar}
         />
+        <Map />
       </div>
     );
   }
