7fac7e23954d399538748c7e91aad588f1c1f7a5
Bycor Sánchez <bycor1989@gmail.com>
2018-07-31 08:28:41 +0100

    feat: create photo info request
    Add method in FlickrAPI to request information about a photo

--- src/utils/FlickrAPI.js
+++ src/utils/FlickrAPI.js
@@ -1,11 +1,31 @@
 const API_KEY = "6d12be5a905a1b0b1f91005b7e63756d";
-const BASE_URL = `http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&format=json&nojsoncallback=1`;
+const BASE_URL = "https://api.flickr.com/services/rest/";
 
-export const searchPhotos = (query, size) => {
-    const requestURL = `${BASE_URL}&text=${query}&per_page=4`;
-    return fetch(requestURL)
+//Search for photos matching that query in Flickr
+export const searchPhotos = (query) => {
+    const request = searchURL(query);
+    return fetch(request)
         .then(res => res.json())
-        .then(data => data.photos.photo.map(p => photoURL(p, size)));
+        .then(data => data.photos.photo);
 };
 
-const photoURL  = ({id, secret, server, farm}, size) => `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_${size}.jpg`;
+//Get info about a photo
+export const photoInfo = (photo) => {
+    const request = infoURL(photo);
+    return fetch(request)
+        .then(res => res.json())
+        .then(data => data.photo);
+};
+
+export const photoURLs = (photos, size) => photos.map(p => photoURL(p, size));
+
+//Get image URL from Flickr response
+export const photoURL  = ({id, secret, server, farm}, size) => `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_${size}.jpg`;
+
+
+//Construct requests dinamycally
+const searchURL = query => `${methodURL("flickr.photos.search")}&text=${query}&per_page=4&sort=relevance`;
+
+const infoURL = ({id}) => `${methodURL("flickr.photos.getInfo")}&photo_id=${id}`;
+
+const methodURL = method => `${BASE_URL}?method=${method}&api_key=${API_KEY}&format=json&nojsoncallback=1`;
