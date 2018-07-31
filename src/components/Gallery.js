1d221ca82a0a49dbd83b21b48163ed1086398224
Bycor Sánchez <bycor1989@gmail.com>
2018-07-31 13:54:14 +0100

    feat: add progress circle

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,50 +1,81 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import * as FlickrAPI from "../utils/FlickrAPI";
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class Gallery extends Component {
+
+	static propTypes = {
+	    galleryOpen: PropTypes.bool.isRequired,
+	    handleClose: PropTypes.func.isRequired
+	};
+
 	state = {
 		status: "loading",
 		photos: []
 	};
 
 	componentDidMount() {
-		const { marker } = this.props;
+		this.loadPhotos(this.props.marker);
+	};
+
+	loadPhotos = marker => {
 		if (!marker) return;
 
 		FlickrAPI.searchPhotos(marker.title)
 			.then(data => {
 				const photos = FlickrAPI.photoURLs(data, "m");
-				this.setState({ photos });
+				this.setState({ photos, status: "loaded" });
+			})
+			.catch(error => {
+				console.log("Images search failed", error);
+				this.setState({ status: "abort" });
 			});
 	};
 
+
 	render(){
-		const { photos } = this.state;
+		const { status, photos } = this.state;
 		const { galleryOpen, handleClose } = this.props;
 
 		return (
 			<Dialog open={ galleryOpen } onClose={ handleClose } aria-labelledby="simple-dialog-title">
 				<DialogTitle id="simple-dialog-title">Photo gallery</DialogTitle>
-				<DialogContent>
-					<GridList cellHeight={160} cols={2}>
-						{	photos && 
-							photos.map((photo, index) => 
-					            (
-                                	<GridListTile key={index}>
-					            		<img src={photo} alt="" />
-					            		<GridListTileBar title="Title" subtitle="Subtitle" />
-					            	</GridListTile>
-					            )
-							)
-						}
-					</GridList>
-				</DialogContent>
+				{	status === "loading" &&
+					(
+						<DialogContent className="gallery-loading">
+							<CircularProgress size={50}/>
+						</DialogContent>
+					)
+				}
+				{	status !== "loading" &&
+					(
+						<DialogContent>
+							<GridList cellHeight={160} cols={2}>
+								{	photos && 
+									photos.map((photo, index) => 
+							            (
+		                                	<GridListTile key={index}>
+							            		<img src={photo} alt="" />
+							            		<GridListTileBar title="Title" subtitle="Subtitle" />
+							            	</GridListTile>
+							            )
+									)
+								}
+							</GridList>
+							<DialogContentText>
+								Images provided by <a href="https://www.flickr.com/">Flickr</a>
+							</DialogContentText>
+						</DialogContent>
+					)
+				}
 			</Dialog>
 		);
 	};
